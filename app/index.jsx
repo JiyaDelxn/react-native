8dbf259e625c5e2240c79b0c6a6a6eed09421cce
JiyaDelxn <singhjiya.delxn@gmail.com>
2025-06-30 18:39:54 +0530

    hello world

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { View,Text } from "react-native";  
+ const Index = () => 
+    {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text  style={{fontSize: 30, fontWeight:"700", color:"black"}}>Hello World </Text>
+            </View>
+            
+        );
+       
+    }      
+    export default Index;
